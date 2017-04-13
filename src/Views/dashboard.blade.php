851b0a5cb9e5d4a7c4cf46bc43cd04830c8dc8ed
24aitor <contact@aitorriba.com>
2017-04-13 02:01:10 +0200

    Tweaks

--- src/Views/dashboard.blade.php
+++ src/Views/dashboard.blade.php
@@ -18,14 +18,14 @@
                 @if (array_key_exists('permission', $widget))
                     @if ($user->hasPermission($widget['permission']) or $user->superAdmin())
                         <div class="uk-width-1-1@m uk-width-1-2@l">
-                            <div class="uk-card uk-card-default uk-card-body">
+                            <div class="uk-card uk-card-default uk-card-body uk-height-1-1">
                                 {!! view($widget['view']) !!}
                             </div>
                         </div>
                     @endif
                 @else
                     <div class="uk-width-1-1@m uk-width-1-2@l">
-                        <div class="uk-card uk-card-default uk-card-body">
+                        <div class="uk-card uk-card-default uk-card-body uk-height-1-1">
                             {!! view($widget['view']) !!}
                         </div>
                     </div>
