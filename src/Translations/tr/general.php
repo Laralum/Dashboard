cb17e2f41a58d80d309d722cd109c969d3ec2554
Tanju Yıldız <ben@tanjuyildiz.com>
2018-02-04 11:18:26 +0300

    Added Turkish translations

---
+++ src/Translations/tr/general.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | General Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'dashboard'     => 'Başlangıç',
+    'home'          => 'Ana Sayfa',
+    'my_dashboard'  => 'Başlangıç',
+    'subtitle'      => 'Merhaba, :name',
+    'no_widgets'    => 'Bileşen yok',
+    'visit_site'    => 'Siteyi Görüntüle',
+    'loading'       => 'Yükleniyor...',
+
+];
